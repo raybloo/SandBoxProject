f8ebe9c520374d0d0b4116afad3df4d1c855023b
raybloo <raybloo@ymail.com>
2019-11-15 18:14:24 +0100

    Project Initialization

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public PlayerManager playerManager;
+
+    public KeyCode MoveForward = KeyCode.W;
+    public KeyCode MoveBackward = KeyCode.S;
+    public KeyCode StrafLeft = KeyCode.A;
+    public KeyCode StrafRight = KeyCode.D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(MoveForward))
+        {
+
+        }
+
+        if (Input.GetKey(MoveBackward))
+        {
+
+        }
+
+        if (Input.GetKey(StrafLeft))
+        {
+
+        }
+
+        if (Input.GetKey(StrafRight))
+        {
+
+        }
+    }
+}
